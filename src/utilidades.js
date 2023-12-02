51221474646dad3082bb18bbc1158186ecb9b689
Willian <wilzghisio@hotmail.com>
2023-12-02 16:16:31 -0300

    Primeiro commit

---
+++ src/utilidades.js
@@ -0,0 +1,339 @@
+export const catalogo = [
+    //cafe
+    {
+      id: "1",
+      nome: "Café Mocha",
+      preco: 7,
+      imagem: "cafe-1.jpg",
+      bebida: false,
+    },
+    {
+      id: "2",
+      nome: "Café Cappuccino",
+      preco: 7,
+      imagem: "cafe-2.jpg",
+      bebida: false,
+    },
+    {
+      id: "3",
+      nome: "Café Expresso",
+      preco: 7,
+      imagem: "cafe-3.jpg",
+      bebida: false,
+    },
+    {
+      id: "4",
+      nome: "Café Macchiato",
+      preco: 7,
+      imagem: "cafe-4.jpg",
+      bebida: false,
+    },
+    {
+      id: "5",
+      nome: "Café com Leite",
+      preco: 7,
+      imagem: "cafe-5.jpg",
+      bebida: false,
+    },
+    {
+      id: "6",
+      nome: "Café Grande",
+      preco: 7,
+      imagem: "cafe-6.jpg",
+      bebida: false,
+    },
+    {
+      id: "27",
+      nome: "Café com Chantilly",
+      preco: 7,
+      imagem: "cafe-7.jpg",
+      bebida: false,
+    },
+  //Salgado
+  {
+    id: "7",
+    nome: "Pão Doce",
+    preco: 5,
+    imagem: "salgaldo-1.jpg",
+    bebida: true,
+  },
+  {
+    id: "8",
+    nome: "Pão Recheado",
+    preco: 5,
+    imagem: "salgaldo-2.jpg",
+    bebida: true,
+  },
+  {
+    id: "9",
+    nome: "Coxinha de Frango",
+    preco: 5,
+    imagem: "salgaldo-3.jpg",
+    bebida: true,
+  },
+  {
+    id: "10",
+    nome: "Beirute",
+    preco: 5,
+    imagem: "salgaldo-4.jpg",
+    bebida: true,
+  },
+  {
+    id: "11",
+    nome: "Pão Cremoso",
+    preco: 5,
+    imagem: "salgaldo-6.jpg",
+    bebida: true,
+  },
+  
+  {
+    id: "13",
+    nome: "Pão de Queijo ",
+    preco: 8,
+    imagem: "salgaldo-7.jpg",
+    bebida: true,
+  },
+  {
+    id: "14",
+    nome: "Pão de Queijo Recheado",
+    preco: 9,
+    imagem: "salgaldo-8.jpg",
+    bebida: true,
+  },
+  {
+    id: "15",
+    nome: "Lanche X-Salada",
+    preco: 5,
+    imagem: "salgaldo-9.jpg",
+    bebida: true,
+  },
+  {
+    id: "16",
+    nome: "Pão de Frios",
+    preco: 7,
+    imagem: "salgaldo-10.jpg",
+    bebida: true,
+  },
+  {
+    id: "17",
+    nome: "Cupcake com Gotas de Chocolate",
+    preco: 5,
+    imagem: "salgaldo-11.jpg",
+    bebida: true,
+  },
+  {
+    id: "18",
+    nome: "Croissant Salgado",
+    preco: 8,
+    imagem: "salgaldo-12.jpg",
+    bebida: true,
+  },
+  {
+    id: "19",
+    nome: "Esfiha de Carne",
+    preco: 8,
+    imagem: "salgaldo-13.jpg",
+    bebida: true,
+  },
+  {
+    id: "33",
+    nome: "Sanduíche Natural",
+    preco: 7,
+    imagem: "salgaldo-14.jpg",
+    bebida: true,
+  },
+  {
+    id: "35",
+    nome: "Coxinha de Frango com Catupiry",
+    preco: 7,
+    imagem: "salgaldo-15.jpg",
+    bebida: true,
+  },
+  {
+    id: "36",
+    nome: "Misto Quente",
+    preco: 7,
+    imagem: "salgaldo-16.jpg",
+    bebida: true,
+  },
+  {
+    id: "37",
+    nome: "Baguete Recheada",
+    preco: 5,
+    imagem: "salgaldo-17.jpg",
+    bebida: true,
+  },
+  
+  //refrigerante
+  {
+    id: "20",
+    nome: "Fanta Laranja",
+    preco: 7,
+    imagem: "refri-1.jpg",
+    bebida: false,
+  },
+  
+  {
+    id: "21",
+    nome: "Schweppers",
+    preco: 7,
+    imagem: "refri-2.jpg",
+    bebida: false,
+  },
+  {
+    id: "22",
+    nome: "Guaraná Antarctica",
+    preco: 7,
+    imagem: "refri-3.jpg",
+    bebida: false,
+  },
+  {
+    id: "23",
+    nome: "Coca Cola",
+    preco: 7,
+    imagem: "refri-4.jpg",
+    bebida: false,
+  },
+  {
+    id: "38",
+    nome: "Fanta Uva",
+    preco: 7,
+    imagem: "refri-5.jpg",
+    bebida: false,
+  },
+  {
+    id: "39",
+    nome: "Guaraná Sprite",
+    preco: 7,
+    imagem: "refri-6.jpg",
+    bebida: false,
+  },
+  {
+    id: "40",
+    nome: "Sucos Naturais",
+    preco: 7,
+    imagem: "refri-7.jpg",
+    bebida: false,
+  },
+  
+  //doce
+    {
+      id: "28",
+      nome: "Rosca Recheada",
+      preco: 7,
+      imagem: "doce-1.jpg",
+      bebida: true,
+    },
+    {
+      id: "24",
+      nome: "Cupcake Recheados",
+      preco: 8,
+      imagem: "doce-2.jpg",
+      bebida: true,
+    },
+    
+
+    {
+      id: "25",
+      nome: "Bolo  de Abacaxi Pedaço",
+      preco: 8,
+      imagem: "doce-5.jpg",
+      bebida: true,
+    },
+    {
+      id: "26",
+      nome: "Bolo de Coco Recheado Pedaço",
+      preco: 8,
+      imagem: "doce-6.jpg",
+      bebida: true,
+    },
+    {
+      id: "29",
+      nome: "Bolo de Chocolate Pedaço",
+      preco: 8,
+      imagem: "doce-7.jpg",
+      bebida: true,
+    },
+    {
+      id: "30",
+      nome: "Carolina Doce de Leite",
+      preco: 8,
+      imagem: "doce-8.jpg",
+      bebida: true,
+    },
+    {
+      id: "31",
+      nome: "Sonho Doce de Leite",
+      preco: 8,
+      imagem: "doce-9.jpg",
+      bebida: true,
+    },
+    {
+      id: "32",
+      nome: "Torta de Morango",
+      preco: 9,
+      imagem: "doce-10.jpg",
+      bebida: true,
+    },
+    
+  
+    
+  ];
+  
+  export function salvarLocalStorage(chave, informacao) {
+    localStorage.setItem(chave, JSON.stringify(informacao));
+  }
+  
+  export function lerLocalStorage(chave) {
+    return JSON.parse(localStorage.getItem(chave));
+  }
+  
+  export function apagarDoLocalStorage(chave) {
+    localStorage.removeItem(chave);
+  }
+  
+  export function desenharProdutoCarrinhoSimples(
+    idProduto,
+    idContainerHtml,
+    quantidadeProduto
+  ) {
+    const produto = catalogo.find((p) => p.id === idProduto);
+    const containerProdutosCarrinho = document.getElementById(idContainerHtml);
+  
+    const elementoArticle = document.createElement("article"); //<article></article>
+    const articleClasses = [
+      "flex",
+      "bg-stone-200",
+      "rounded-lg",
+      "p-1",
+      "relative",
+      "mb-2",
+      "w-96",
+    ];
+  
+    for (const articleClass of articleClasses) {
+      elementoArticle.classList.add(articleClass);
+    }
+    //<article class="flex bg-slate-100 rounded-lg p-1 relative"></article>
+  
+    const cartaoProdutoCarrinho = `
+      <img
+        src="assents/img/${produto.imagem}"
+        alt="Carrinho: ${produto.nome}"
+        class="h-24 rounded-lg"
+      />
+      <div class="p-2 flex flex-col justify-between">
+        <p class="text-slate-900 text-sm">
+          ${produto.nome}
+        </p>
+        <p class="text-green-700 text-lg">$${produto.preco}</p>
+      </div>
+      <div class='flex text-slate-950 items-end absolute bottom-0 right-2 text-lg'>
+          <p id='quantidade-${produto.id}' class='ml-2'>${quantidadeProduto}</p>
+      </div>`;
+    //<article class="flex bg-slate-100 rounded-lg p-1 relative">codigo do cartao do produto</article>
+  
+    elementoArticle.innerHTML = cartaoProdutoCarrinho;
+    containerProdutosCarrinho.appendChild(elementoArticle);
+  }
+  
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import tailwindcss from "tailwindcss";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+   plugins: [],
+   base: "/Barista",
+    resolve: {
+    /*something*/
+  },
+
+  
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        checkout: resolve(__dirname, "./checkout.html"),
+        pedidos: resolve(__dirname, "./pedidos.html"),
+      },
+    },
+  },
+});
